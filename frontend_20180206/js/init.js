ec7ebf0a17cd720e548a1307542a464156bbefea
uxui1st <30430944+uxui1st@users.noreply.github.com>
2018-03-20 18:03:32 +0900

    Delete init.js

--- frontend_20180206/js/init.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
-	$('.mockup a').on('click',function(e){
-		e.preventDefault();
-		thisURL=$(this).attr('href');
-		window.open(thisURL,'','width=380,height=700,left=300,top=10')
-	})
-	
-});
